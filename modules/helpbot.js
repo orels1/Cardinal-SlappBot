6c6f0bf11a76077ff15de070049b2c2fd6c72aa1
orels1 <orel-s1@yandex.ru>
2016-10-06 03:33:25 +0300

    refactor

--- modules/helpbot.js
+++ modules/helpbot.js
@@ -8,69 +8,62 @@ slapp.route('searchHelp', (msg) => {
     let test = new RegExp(["фичер", "тест", "викторин", "гиф"].join('\S*|'));
     if (test.test(msg.body.event.text)) {
         let result = test.exec(msg.body.event.text);
-        console.log('Result', result);
-        let state = { section: result[0] };
-        msg.say('Хорошо, но давай уточним').route('sectionSelect', state, 60);
+        let response;
+
+        // Options go here for time being
+        // TODO: Do something about that. Better move to a .json file
+        switch (result[0]) {
+            case 'фичер':
+                response = [
+                    "Как поставить фичер",
+                    "Какие размеры у картинок",
+                    "Рекомендации по подбору картинок",
+                    "Что такое концентрированный фичер",
+                    "Примеры"
+                ];
+                break
+            case 'тест':
+            case 'викторин':
+                response = [
+                    "Типы викторин",
+                    "Как сделать текстовую викторину",
+                    "Как сделать визуальную викторину",
+                    "Как сделать аудио-викторину",
+                    "Как сделать викторину «Узнай, кто ты»",
+                    "Как добавить викторину в статью",
+                    "Примеры"
+                ];
+                break
+            case 'гиф':
+                response = [
+                    "Как сделать гифку из видео",
+                    "Требования к размерам и весу гифок на сайте",
+                    "Как облегчить гифку",
+                    "Где искать гифки",
+                    "Примеры"
+                ];
+                break
+            default:
+                msg.say(':round_pushpin: Что-то пошло не так :cold_sweat:');
+                return
+                break
+        }
+
+        // Build attachments
+        let options = [];
+        response.forEach((item, index) => {
+            options.push({
+                text: index + '. ' + item
+            });
+        });
+        msg.say({
+            text: "Хорошо, но давай уточним. Что именно тебя интересует?",
+            attachments: options
+        });
         return
+        
     } else {
         msg.say('К сожалению, я не могу с этим помочь :cold_sweat:');
         return
     }
-});
-
-slapp.route('sectionSelect', (msg, state) => {
-    console.log('In da route!');
-    let response;
-
-    switch (state.section) {
-        case 'фичер':
-            response = [
-                "Как поставить фичер",
-                "Какие размеры у картинок",
-                "Рекомендации по подбору картинок",
-                "Что такое концентрированный фичер",
-                "Примеры"
-            ];
-            break
-        case 'тест':
-        case 'викторин':
-            response = [
-                "Типы викторин",
-                "Как сделать текстовую викторину",
-                "Как сделать визуальную викторину",
-                "Как сделать аудио-викторину",
-                "Как сделать викторину «Узнай, кто ты»",
-                "Как добавить викторину в статью",
-                "Примеры"
-            ];
-            break
-        case 'гиф':
-            response = [
-                "Как сделать гифку из видео",
-                "Требования к размерам и весу гифок на сайте",
-                "Как облегчить гифку",
-                "Где искать гифки",
-                "Примеры"
-            ];
-            break
-        default:
-            msg.say(':round_pushpin: Что-то пошло не так :cold_sweat:');
-            return
-            break
-    }
-    
-    let options = [];
-    response.forEach((item, index) => {
-        options.push({
-            text: index + '. ' + item
-        });
-    });
-
-    console.log('Response', response, 'Options', options);
-
-    msg.say({
-        text: "Что именно тебя интересует?",
-        attachments: options
-    });
-    return
 });
