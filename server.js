59015156900332a1299862e8ede21be51f882f12
orels1 <orel-s1@yandex.ru>
2016-10-09 01:03:23 +0300

    shouts sending

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 'use strict'
 const express = require('express');
+const request = require('express');
 //if(!process.env.PORT) throw Error('PORT is missing but required')
 
 /*
@@ -29,6 +30,75 @@ slapp.message('^попрошу.*', ['mention'], (msg) => {
     msg.say(":oldfag::tophat:\n:point_up::peka::wine_glass:");
 })
 
+slapp.message('.*напиши вопль.*', ['mention', 'direct_message'], (msg) => {
+    msg.say('Что написать?').route('sendShout', 60);
+})
+
+slapp.route('sendShout', (msg) => {
+    if (msg.body.event.type != 'message') {
+        msg.say('Пожалуйста, напиши текст').route('sendShout', 60);
+        return
+    } else if (msg.body.event.text.length > 256) {
+        msg.say('Извини, текст слишком длинный, постарайся убрать '
+                + (msg.body.event.text.length - 256) + 'символов')
+        .route('sendShout', 60);
+        return
+    } else {
+        // Configure form-data for shouts
+        let formData = {
+            'style': 'tiny',
+            'cry-text': msg.body.event.text,
+            'cry_path': '/'
+        }
+
+        // Configure cookies, those will need to be updated from time to time,
+        // but they are pretty liberal
+
+        let j = request.jar();
+        let url = "http://kanobu.ru"
+        j.setCookie(request.cookie('csrftoken=' + process.env.CSRF_TOKEN), url);
+        j.setCookie(request.cookie('probtnid=' + process.env.PROBTN_ID), url);
+        j.setCookie(request.cookie('gsid=' + process.env.GS_ID), url);
+
+        request.post({url: url + '/shouts/add/', jar: j, formData: formData},
+            (err, response, body) => {
+                if (err) {
+                    // if crashed - ask for retry
+                    msg.say('Что-то пошло не так, попробуем еще раз? (да/нет/+/-)')
+                    .route('shoutRetry', 60);
+                    return console.error('posting failed', err);
+                } else if (response.statusCode != 200) {
+                    // if request was good, but something went wrong
+                    msg.say('Канобу вернул ошибку, попробуем еще раз? (да/нет/+/-)')
+                    .route('shoutRetry', 60);
+                    return
+                } else {
+                    // if everything is good
+                    msg.say('Вопль успешно отправлен ^^');
+                    return
+                }
+            })
+    }
+});
+
+// retry handle
+slapp.route('shoutRetry', (msg) => {
+    if (msg.body.event.type != 'message') {
+        msg.say('Пожалуйста, используй да/нет/+/-').route('shoutRetry', 60);
+        return
+    } else if (msg.body.event.text == 'да' || msg.body.event.text == '+') {
+        msg.say('Хорошо, что напишем?')
+        .route('sendShout', 60);
+        return
+    } else if (msg.body.event.text == 'нет' || msg.body.event.text == '-') {
+        msg.say('Хорошо, не будем ничего писать');
+        return
+    } else {
+        msg.say('Пожалуйста, используй да/нет/+/-').route('shoutRetry', 60);
+        return
+    }
+})
+
 /*
  *  END OF SLAPP
  */
@@ -38,11 +108,6 @@ app.get('/', function(req, res) {
 });
 
 // Debug some stuff here
-
-console.log('Tokens will go next\n',
-            'CSRF_TOKEN', process.env.CSRF_TOKEN,
-            'PROBTN_ID', process.env.PROBTN_ID,
-            'GS_ID', process.env.GS_ID);
 
 app.listen(app.get('port'), function() {
   console.log('Express server listening on port ' + app.get('port'));
