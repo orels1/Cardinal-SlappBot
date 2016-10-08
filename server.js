d50cb156b60d3647bf6a0887ac52230d5be3f93c
orels1 <orel-s1@yandex.ru>
2016-10-09 00:31:35 +0300

    added special tokens config

--- server.js
+++ server.js
@@ -25,44 +25,6 @@ const helpbot = require('./modules/helpbot');
 
 // do stuff
 
-slapp.message('^(hi|hello|\:wave\:|привет|хэй).*', ['mention'], (msg) => {
-  msg.say('Да-да... Я тут... Хотел чего-то?').route('handleRequest', 60);
-});
-
-slapp.message('^спаси.*', ['mention'], (msg) => {
-  msg.say('orels1 сбежал... ¯\\_(ツ)_/¯');
-});
-
-slapp.route('handleRequest', (msg) => {
-    let test  = ["топ", "рейтинг", "top", "лидерборд"];
-    if (new RegExp(test.join('|')).test(msg.body.event.text)) {
-        msg.say('Лагуна все еще лучший ¯\\_(ツ)_/¯');
-        return
-    } else {
-        msg.say('Ну и ладно');
-        return
-    }
-});
-
-slapp.message("бот, ешь картинку", (msg) => {
-    msg.say('Не хочу >.<').route('handleImageUpload');
-});
-
-slapp.route('handleImageUpload', (msg) => {
-    if (msg.body.event.subtype = "file_share") {
-        msg.say({
-            text: "Я тож так могу",
-            attachments: [
-                {
-                    text: "",
-                    image_url: msg.body.event.file.permalink
-                }
-            ]
-        });
-    }
-
-});
-
 slapp.message('^попрошу.*', ['mention'], (msg) => {
     msg.say(":oldfag::tophat:\n:point_up::peka::wine_glass:");
 })
@@ -74,6 +36,11 @@ slapp.message('^попрошу.*', ['mention'], (msg) => {
 app.get('/', function(req, res) {
     res.send('Alive!');
 });
+
+// Debug some stuff here
+
+console.log('Tokens will go next\n', 'CSRF_TOKEN', CSRF_TOKEN, 'PROBTN_ID', PROBTN_ID,
+            'GS_ID', GS_ID);
 
 app.listen(app.get('port'), function() {
   console.log('Express server listening on port ' + app.get('port'));
