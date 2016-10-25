d1cbcf1916bc85537be5a43c7492b39a7be15cc5
orels1 <orel-s1@yandex.ru>
2016-10-25 18:05:51 +0300

    welcome

--- modules/fun.js
+++ modules/fun.js
@@ -5,3 +5,7 @@ var slapp = require('./init');
 slapp.message('^попрошу.*', ['mention'], (msg) => {
     msg.say(":oldfag::tophat:\n:point_up::peka::wine_glass:");
 })
+
+slapp.message('^Поздоровайся.*', ['mention'], (msg) => {
+    msg.say("Привет! Я могу помочь освоиться с нашими инструментами. Просто напиши \"@cardinal помоги\" и я сделаю все, что в моих силах :)");
+})
