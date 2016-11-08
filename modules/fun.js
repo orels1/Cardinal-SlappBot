f914918f62c815925b7571a678525331f1d98a8f
orels1 <orel-s1@yandex.ru>
2016-11-08 14:13:44 +0300

    fun, i guess?

--- modules/fun.js
+++ modules/fun.js
@@ -6,6 +6,6 @@ slapp.message('^попрошу.*', ['mention'], (msg) => {
     msg.say(":oldfag::tophat:\n:point_up::peka::wine_glass:");
 })
 
-slapp.message('^Поздоровайся.*', ['mention'], (msg) => {
+slapp.message('^поздоровайся.*', ['mention'], (msg) => {
     msg.say("Привет! Я могу помочь освоиться с нашими инструментами. Просто напиши \"@cardinal помоги\" и я сделаю все, что в моих силах :)");
 })
